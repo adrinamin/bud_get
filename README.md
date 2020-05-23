c58b156d9e763f9b13325798e2d362a7e6565910
adrinamin <adrin.amin@gmail.com>
2020-05-23 21:29:43 +0200

    adding user interaction.

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # bud_get
 
 Coming soon...
+
+for using this app install the libraries first:
+
+`python -m pip install -r requirements.txt`
--- source/budget.py
+++ source/budget.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, abort, jsonify
-from model import db
+from flask import (Flask, render_template, abort, jsonify, request, redirect, url_for)
+from model import db, save_db
 
 app = Flask(__name__)
 
@@ -9,13 +9,57 @@ def welcome():
     return render_template("welcome.html", accounts=db)
 
 
-@app.route("/account/<int:index>")
-def account(index):
+@app.route("/add_account", methods=["GET", "POST"])
+def add_account():
+    if request.method == "POST":
+        account = {"name": request.form["name"], "account": request.form["account"], "budgets": []}
+        db.append(account)
+        save_db()
+        return redirect(url_for("welcome"))
+    else:
+        return render_template("add_account.html")
+
+
+@app.route("/remove_account/<int:index>", methods=["GET", "POST"])
+def remove_account(index):
     try:
-        account = db[index]
-        return render_template("account.html", account=account, index=index, max_index=len(db)-1)
+        if request.method == "POST":
+            del db[index]
+            save_db()
+            return redirect(url_for("welcome"))
+        else:
+            return render_template("remove_account.html", account=db[index])
     except IndexError:
         abort(404)
+
+
+@app.route("/account/<int:index>")
+def account(index):
+    account = {}
+    budgets = []
+    try:
+        account = db[index]
+        budgets = account["budgets"]
+        return render_template("account.html", account=account, budgets=budgets, index=index, max_index=len(db)-1, budget_count=len(budgets))
+    except IndexError:
+        abort(404)
+
+
+@app.route("/add_budget/<int:index>", methods=["GET", "POST"])
+def add_budget(index):
+    account = {}
+    budgets = []
+    account = db[index]
+    if request.method == "POST":
+        # form has been submitted, process data
+        budget = {"name": request.form["name"], "amount": request.form["amount"]}
+        budgets = account["budgets"]
+        budgets.append(budget)
+        # account["budgets"] = budgets
+        save_db()
+        return redirect(url_for("account", index=len(db)-1))
+    else:
+        return render_template("add_budget.html", index=index)
 
 
 @app.route("/api/accounts")
@@ -29,15 +73,3 @@ def api_account(index):
         return db[index]
     except IndexError:
         abort(404)
-
-# @app.route("/budget")
-# def budget():
-#     return "This is the budget page"
-
-# counter = 0
-
-# @app.route("/counter")
-# def count_demo():
-#     global counter
-#     counter += 1
-#     return "This page was served " + str(counter) + " times"
