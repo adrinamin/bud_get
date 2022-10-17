681d9bb6e9ab571813870ce0c67f3ab43494b358
Adrin Amin Salehi <adrin.amin@gmail.com>
2022-10-17 08:52:12 +0000

    remove old code

--- README.md
+++ README.md
@@ -1,7 +1,3 @@
 # bud_get
 
 Coming soon...
-
-for using this app install the libraries first:
-
-`python -m pip install -r requirements.txt`
--- source/budget.mobile/budget_mobile/android/app/src/main/kotlin/com/example/budget_mobile/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.budget_mobile
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
