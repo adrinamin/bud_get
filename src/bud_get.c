c74e6146ccf6871205bd03c8f8bfb14c5d669b26
Adrin Amin Salehi <adrin.amin@gmail.com>
2024-01-05 11:21:45 +0000

    Implement main menu

--- src/bud_get.c
+++ src/bud_get.c
@@ -1,9 +1,26 @@
 #include <stdio.h>
 
-void main_menu();
+int main_menu();
 void help_page();
+void create_account();
+void get_accounts();
+void get_budgets();
 
 int main(int argc, char *argv[]) {
+
+  if (argc == 1) {
+    int option = main_menu();
+
+    switch (option) {
+    case 1:
+      create_account();
+      break;
+    default:
+      printf("bud_get: invalid option -- '%i'\n", option);
+      break;
+    }
+    return 0;
+  }
 
   switch (argv[1][1]) {
   case 'a':
@@ -36,3 +53,23 @@ void help_page() {
   printf("  -h,\tdisplay this help and exit\n");
   printf("\n");
 }
+
+int main_menu() {
+  printf("Welcome to bud_get!\n");
+  printf("What do you want to do?\n");
+  printf("\n");
+  printf("Choose one of the following numbers:\n");
+  printf("1. Create a new account\n");
+  printf("2. Create a new budget\n");
+  printf("3. List accounts\n");
+  printf("4. List budgets\n");
+  printf("5. Exit\n");
+
+  printf("Option: ");
+  int option;
+  scanf("%i", &option);
+  printf("You chose %i\n", option);
+  return option;
+}
+
+void create_account() { printf("Creating a new account...\n"); }
