428d15f205342a59a91efd1a53b059b7c224ebb9
Adrin Amin Salehi <adrin.amin@gmail.com>
2024-01-05 21:45:24 +0000

    Implement basic account creation

--- src/bud_get.c
+++ src/bud_get.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main_menu();
 void help_page();
 void create_account();
 void get_accounts();
 void get_budgets();
+static void create_file(char *filename);
 
 int main(int argc, char *argv[]) {
 
@@ -72,4 +74,37 @@ int main_menu() {
   return option;
 }
 
-void create_account() { printf("Creating a new account...\n"); }
+void create_account() {
+  printf("Creating a new account...\n");
+
+  // Open file if exists, create it if it doesn't
+  create_file("accounts.csv");
+
+  // Open file
+  FILE *file = fopen("accounts.csv", "a+");
+
+  // Check if file is empty
+  fseek(file, 0, SEEK_END);
+
+  // Write header if file is empty
+  long size = ftell(file);
+  if (size == 0) {
+    fprintf(file, "Account, Bank, Holder, Amount\n");
+  }
+
+  // Write account
+  fprintf(file, "Account 2, My bank, My name, 1000\n");
+
+  fclose(file);
+}
+
+static void create_file(char *filename) {
+  FILE *file = fopen(filename, "a+");
+
+  if (file == NULL) {
+    printf("Error opening/creating the file.\n");
+    exit(0);
+  }
+
+  fclose(file);
+}
