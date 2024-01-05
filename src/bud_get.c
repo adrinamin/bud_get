9b30733735378a269bdec4c0cf5f68b3ce532c03
Adrin Amin Salehi <adrin.amin@gmail.com>
2024-01-05 06:58:40 +0000

    Implement basic main function

--- src/bud_get.c
+++ src/bud_get.c
@@ -1,7 +1,38 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main (void)
-{
-    printf("Hello world\n");
-    return 0;
+void main_menu();
+void help_page();
+
+int main(int argc, char *argv[]) {
+
+  switch (argv[1][1]) {
+  case 'a':
+    printf("Listing accounts...\n");
+    break;
+
+  case 'b':
+    printf("Listing budgets...\n");
+    break;
+
+  case 'h':
+    help_page();
+    break;
+
+  default:
+    printf("bud_get: invalid option -- '%c'\n", argv[1][0]);
+    printf("Try 'bud_get -h' for more information.\n");
+    break;
+  }
+
+  return 0;
+}
+
+void help_page() {
+  printf("Usage: bud_get [OPTION]...\n");
+  printf("List accounts or budgets.\n");
+  printf("\n");
+  printf("  -a,\tlist accounts\n");
+  printf("  -b,\tlist budgets\n");
+  printf("  -h,\tdisplay this help and exit\n");
+  printf("\n");
 }
